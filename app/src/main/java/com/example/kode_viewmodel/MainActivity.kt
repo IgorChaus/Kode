f417cbc4000b8b144d5e523ce0d16e585812a0c8
Igor Chaus <igor_chaus@mail.ru>
2023-02-04 16:34:49 +0200

    Added SnackBar

--- app/src/main/java/com/example/kode_viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/kode_viewmodel/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.kode_viewmodel.model.Person
 import com.example.kode_viewmodel.model.Resource
 import com.example.kode_viewmodel.vm.AppViewModel
+import com.google.android.material.snackbar.Snackbar
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -41,18 +42,35 @@ class MainActivity : AppCompatActivity() {
             viewModel.fetchPersons()
         }
 
+        val snackbarLoading: Snackbar = Snackbar.make(rv,"Секундочку, гружусь...", Snackbar
+            .LENGTH_INDEFINITE)
+        snackbarLoading.setBackgroundTint(ResourcesCompat.getColor(resources,R.color.purple,null))
+        snackbarLoading.setTextColor(ResourcesCompat.getColor(resources,R.color.white,null))
+
+        val snackbarError: Snackbar = Snackbar.make(rv,"""Не могу обновить данные.
+            |Проверьте соединение с Интернетом.""".trimMargin(), Snackbar.LENGTH_LONG)
+        snackbarError.setBackgroundTint(ResourcesCompat.getColor(resources,R.color.red,null))
+        snackbarError.setTextColor(ResourcesCompat.getColor(resources,R.color.white,null))
+
         viewModel.itemsLiveData.observe(this, Observer {
             when(it){
                 is Resource.Success -> {
                     adapter.refreshUsers(it.data?.items as ArrayList<Person.Items>)
                     swipeContainer.isRefreshing = false
-                }
-                is Resource.Error -> {
-                    val intent = Intent(this@MainActivity,ErrorActivity::class.java)
-                    startActivity(intent)
+                    snackbarLoading.dismiss()
                 }
 
-                is Resource.Loading -> Log.i("MyTag", "Loading")
+                is Resource.Error -> {
+                    if(it.message == "IOException"){
+                        snackbarError.show()
+                        swipeContainer.isRefreshing = false
+                    }else{
+                        val intent = Intent(this@MainActivity,ErrorActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+
+                is Resource.Loading -> snackbarLoading.show()
 
             }
         })
