6ba3cabd346357c1ca10b274d62804229e3d3251
Igor Chaus <igor_chaus@mail.ru>
2023-02-09 08:38:43 +0200

    Added all elements MainActivity

--- app/src/main/java/com/example/kode_viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/kode_viewmodel/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.kode_viewmodel
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,12 +22,39 @@ import com.example.kode_viewmodel.model.Person
 import com.example.kode_viewmodel.model.Resource
 import com.example.kode_viewmodel.source.DataRepository
 import com.example.kode_viewmodel.vm.AppViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
+    private var tabName: String = "Все"
+    private var strSearch: String =""
+
+    private var checkedBotton: Int = R.id.radioButton1
+
+    companion object {
+        val departments = mapOf(
+            "Все" to "All",
+            "Android" to "android",
+            "iOS" to "ios",
+            "Дизайн" to "design",
+            "Менеджмент" to "management",
+            "QA" to "qa",
+            "Бэк-офис" to "back_office",
+            "Frontend" to "frontend",
+            "HR" to "hr",
+            "PR" to "pr",
+            "Backend" to "backend",
+            "Техподдержка" to "support",
+            "Аналитика" to "analytics"
+        )
+    }
 
     private val dataRepository = DataRepository()
     val factory = AppViewModel.Factory(dataRepository)
@@ -38,6 +73,128 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = RAdapter()
         rv.adapter = adapter
+
+        window.setBackgroundDrawable(
+            ContextCompat
+            .getDrawable(this@MainActivity,R.color.white))
+
+        //--------------------- SEARCH ----------------------
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val editText: EditText = findViewById(R.id.editText)
+
+        val sortButton: ImageButton = findViewById(R.id.imageButton)
+        sortButton.setOnClickListener {
+            Log.i("MyTag",sheetBehavior.state.toString())
+            when(sheetBehavior.state){
+                BottomSheetBehavior.STATE_COLLAPSED ->
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+                BottomSheetBehavior.STATE_EXPANDED ->
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED)
+                else -> TODO()
+            }
+        }
+
+        val buttonCancel: Button = findViewById(R.id.button)
+        buttonCancel.setOnClickListener {
+            editText.clearFocus()
+            buttonCancel.visibility = View.GONE
+            strSearch = ""
+            editText.setText(strSearch)
+
+            //Hide keyboard
+            val  imm = editText.context.getSystemService(
+                Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(editText.windowToken, 0)
+
+   //         setFilter(rv,adapter)
+            sortButton.visibility = View.VISIBLE
+
+            editText.setCompoundDrawablesWithIntrinsicBounds(
+                ResourcesCompat.getDrawable(resources, R.drawable.icon_search, null),
+                null, null, null)
+
+        }
+
+        editText.addTextChangedListener {
+                s ->  strSearch = s.toString()
+   //         setFilter(rv,adapter)
+        }
+
+        editText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                buttonCancel.visibility = View.VISIBLE
+                sortButton.visibility = View.GONE
+                editText.setCompoundDrawablesWithIntrinsicBounds(
+                    ResourcesCompat.getDrawable(resources, R.drawable.icon_search_black, null),
+                    null, null, null)
+            } else {
+                buttonCancel.visibility = View.GONE
+                sortButton.visibility = View.VISIBLE
+            }
+        }
+
+
+        //---------------- TABS ---------------------------
+        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        departments.forEach{
+            tabLayout.addTab(tabLayout.newTab().setText(it.key))
+        }
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tabName = tab?.text.toString()
+  //              setFilter(rv,adapter)
+            }
+
+        })
+
+        //----------------- BOTTOM SHEET --------------------------------
+        val bottomSheet: ConstraintLayout = findViewById(R.id.bottomSheet)
+        sheetBehavior = BottomSheetBehavior.from(bottomSheet)
+
+        sheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+                val checkBotton: RadioButton = findViewById(checkedBotton)
+                checkBotton.isChecked = true
+
+                when (newState) {
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        window.setBackgroundDrawable(ContextCompat
+                            .getDrawable(this@MainActivity,R.color.grey_transpar))
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        window.setBackgroundDrawable(ContextCompat
+                            .getDrawable(this@MainActivity,R.color.white))
+                    }
+                    else -> return
+                }
+            }
+        })
+
+        val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
+        radioGroup.clearCheck()
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            checkedBotton = checkedId
+    //        setFilter(rv, adapter)
+            if (checkedBotton == R.id.radioButton2) {
+                sortButton.setImageResource(R.drawable.icon_right_purple)
+            } else {
+                sortButton.setImageResource(R.drawable.icon_right)
+            }
+            sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+
+
 
         val swipeContainer: SwipeRefreshLayout = findViewById(R.id.swipe_refresh_layout)
         swipeContainer.setProgressBackgroundColorSchemeColor(ResourcesCompat.getColor(resources,
