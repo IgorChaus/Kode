39c63587b28479c4a364f19fe2cd265465d92411
Igor Chaus <igor_chaus@mail.ru>
2023-04-08 09:45:18 +0200

    Refactored ErrorScreen using viewBinding

--- app/src/main/java/com/example/kode_viewmodel/view/ErrorScreen.kt
+++ app/src/main/java/com/example/kode_viewmodel/view/ErrorScreen.kt
@@ -9,8 +9,11 @@ import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.kode_viewmodel.R
+import com.example.kode_viewmodel.databinding.ErrorScreenBinding
+import com.example.kode_viewmodel.databinding.ScreenItemBinding
 
 class ErrorScreen : Fragment() {
+    private var binding: ErrorScreenBinding? = null
 
     companion object {
         fun getIstance() = ErrorScreen()
@@ -20,17 +23,24 @@ class ErrorScreen : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?): View {
+        savedInstanceState: Bundle?): View? {
 
-        val view = inflater.inflate(R.layout.error_screen, container, false)
+  //      val view = inflater.inflate(R.layout.error_screen, container, false)
+        binding = ErrorScreenBinding.inflate(inflater, container, false)
 
-        val textView: TextView = view.findViewById(R.id.textView3)
-        textView.setOnClickListener {
+ //       val textView: TextView = view.findViewById(R.id.textView3)
+        binding?.textView3?.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()
                 ?.replace(R.id.container_activity, MainScreen.getInstance())
                 ?.commit()
         }
 
-        return view
+        return binding?.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
 }
