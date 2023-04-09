b9293b679d9749b0ffdf7de0c3eba30baf5b39bd
Igor Chaus <igor_chaus@mail.ru>
2023-04-09 07:19:40 +0200

    Deleted unused libraries

--- app/src/main/java/com/example/kode_viewmodel/view/ItemScreen.kt
+++ app/src/main/java/com/example/kode_viewmodel/view/ItemScreen.kt
@@ -6,9 +6,6 @@ import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
@@ -77,6 +74,7 @@ class ItemScreen : Fragment() {
         }
         return ""
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
