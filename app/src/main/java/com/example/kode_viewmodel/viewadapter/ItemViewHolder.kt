66c2c41b983a19cfff25fea9185e164b162c2cdb
Igor Chaus <igor_chaus@mail.ru>
2023-10-12 21:29:25 +0200

    Refactored binding in View Holders

--- app/src/main/java/com/example/kode_viewmodel/viewadapter/ItemViewHolder.kt
+++ app/src/main/java/com/example/kode_viewmodel/viewadapter/ItemViewHolder.kt
@@ -1,13 +1,6 @@
 package com.example.kode_viewmodel.viewadapter
 
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kode_viewmodel.databinding.ItemBinding
 
-class ItemViewHolder(binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
-    val personPhoto: ImageView = binding.imageView
-    val personName: TextView = binding.personName
-    val personTag: TextView = binding.personTag
-    val personDepartment: TextView = binding.personDepartment
-}
+class ItemViewHolder(val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root)
