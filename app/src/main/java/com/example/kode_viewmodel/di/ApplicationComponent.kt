5cd247a204e0c5f6cd1a7487babb536f760230d7
Igor Chaus <igor_chaus@mail.ru>
2024-02-29 21:09:57 +0200

    Added ApplicationScope

--- app/src/main/java/com/example/kode_viewmodel/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/kode_viewmodel/di/ApplicationComponent.kt
@@ -3,6 +3,7 @@ package com.example.kode_viewmodel.di
 import com.example.kode_viewmodel.view.MainScreen
 import dagger.Component
 
+@ApplicationScope
 @Component(modules = [DataModule::class])
 interface ApplicationComponent {
 
